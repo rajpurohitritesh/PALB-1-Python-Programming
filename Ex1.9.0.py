47cdca01ee61d16deb6ddfa61015551f8ddc987c
Ritesh Rajpurohit 👨🏻‍💻 <rajpurohitritesh05@gmail.com>
2026-03-16 00:50:13 +0530

    Refactor searchInsert to twoSum function

--- Ex1.9.0.py
+++ Ex1.9.0.py
@@ -1,20 +1,14 @@
-#Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
-#You must write an algorithm with O(log n) runtime complexity.
+#Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 class Solution:
-    def searchInsert(self, nums: list[int], target: int) -> int:
-        left = 0
-        right = len(nums) - 1
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        seen = {}
 
-        while left <= right:
-            mid = (left + right) // 2
+        for i in range(len(nums)):
+            complement = target - nums[i]
 
-            if nums[mid] == target:
-                return mid
-            elif nums[mid] < target:
-                left = mid + 1
-            else:
-                right = mid - 1
+            if complement in seen:
+                return [seen[complement], i]
 
-        return left
+            seen[nums[i]] = i
 
         
